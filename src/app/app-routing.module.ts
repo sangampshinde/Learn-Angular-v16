68a809566a17f37e157d3129bb8a4d11f1dffb7e
sangam shinde <sangampshinde@gmail.com>
2024-06-23 10:08:58 +0530

    rchild route setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,33 @@ import { AboutComponent } from './Routing/about/about.component';
 import { UserComponent } from './Routing/user/user.component';
 import { HomeComponent } from './Routing/home/home.component';
 import { PageNotFoundComponent } from './Routing/page-not-found/page-not-found.component';
+import { AboutCompanyComponent } from './Routing/Child_Routing/about-company/about-company.component';
+import { AboutMeComponent } from './Routing/Child_Routing/about-me/about-me.component';
 
 
 // routing define area to define routes
 const routes: Routes = [
   {
+    // Note dont put / infront of routes here wile defining path
+    path:'',
+    component:HomeComponent,
+  },
+
+  {
     path:'about',
     component:AboutComponent,
+    // child Route array 
+    children:[
+      {path:'company',component:AboutCompanyComponent},
+      {path:'me',component:AboutMeComponent}
+    ]
   },
+
   {
     path:'user/:id',
     component:UserComponent,
   },
-  {
-    path:'',
-    component:HomeComponent,
-  },
+
   {
     path:'**',//wild card route
     component:PageNotFoundComponent,
