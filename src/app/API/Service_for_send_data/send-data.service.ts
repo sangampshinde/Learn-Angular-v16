acf34607a68a80061547a861bba26331ae985963
sangam shinde <sangampshinde@gmail.com>
2024-07-07 21:45:58 +0530

    sendata to api

--- src/app/API/Service_for_send_data/send-data.service.ts
+++ src/app/API/Service_for_send_data/send-data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SendDataService {
+  url="http://localhost:8000/heros";
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  sendData(data:any)
+  {
+    return this.http.post(this.url, data);
+  }
+
 }
--- src/app/API/senddata/senddata.component.ts
+++ src/app/API/senddata/senddata.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SendDataService } from '../Service_for_send_data/send-data.service';
 
 @Component({
   selector: 'app-senddata',
@@ -6,9 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./senddata.component.css']
 })
 export class SenddataComponent {
+  constructor(private senddata: SendDataService) { }
 
   user_form_data(data: any) {
     console.log(data);
+    this.senddata.sendData(data).subscribe((data)=>{
+      console.log('Data sent successfully', data);
+    }); // Send data to the service
 
   }
  
